237cd0fa5e2b80f8c6f75656611b7cbae67615a0
António Fialho <aluno21895@ipt.pt>
2021-03-22 11:20:25 +0000

    Commit Inicial

---
+++ Calculadora_tA-B/Calculadora_tA-B/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculadora_tA_B.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
